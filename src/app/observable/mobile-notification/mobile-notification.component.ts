acf2b917fe99d4a2e2d34d01850432443e22158c
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-07-16 22:56:22 +0530

    completed the mobile notification work using concatMap

--- src/app/observable/mobile-notification/mobile-notification.component.ts
+++ src/app/observable/mobile-notification/mobile-notification.component.ts
@@ -16,31 +16,31 @@ export class MobileNotificationComponent implements OnInit {
       name: 'X',
       icon: 'assets/x-icon.png',
       time: '4 Second Ago.',
-      img: 'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-1170x780.jpg',
-      strong: 'Alax Johnson',
-      p: 'Commented on your #Uxtrendz Post: Awesome Post !!! Thank You!'
+      img: 'https://deep-image.ai/_next/static/media/app-info-generator.bf08e63e.webp',
+      strong: 'James Smith',
+      p: 'Twitted : Lorem ipsus dolor sit amet consctetur adipisicing elit.'
     },
     {
       name: 'Facebook',
       icon: 'assets/fb-icon.png',
-      time: '4 Second Ago.',
-      img: 'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-1170x780.jpg',
-      strong: 'Alax Johnson',
-      p: 'Commented on your #Uxtrendz Post: Awesome Post !!! Thank You!'
+      time: '3 Second Ago.',
+      img: 'https://akm-img-a-in.tosshub.com/indiatoday/images/media_bank/202307/world-nature-conservation-day-2023-280643-16x9.jpg?VersionId=887GSLlkezjNnkoZjQ3BoD9IrQFSUs5M&size=690:388',
+      strong: 'David Smith',
+      p: 'Posted : Auteem dolores qui tempora, expedita, doloribus similique que nam consctetur.'
     },
     {
       name: 'X',
       icon: 'assets/x-icon.png',
-      time: '4 Second Ago.',
+      time: '2 Second Ago.',
       img: 'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-1170x780.jpg',
-      strong: 'Alax Johnson',
+      strong: 'James Smith',
       p: 'Commented on your #Uxtrendz Post: Awesome Post !!! Thank You!'
     },
     {
       name: 'Facebook',
       icon: 'assets/fb-icon.png',
-      time: '4 Second Ago.',
-      img: 'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-1170x780.jpg',
+      time: '1 Second Ago.',
+      img: 'https://cf2.bstatic.com/xdata/images/hotel/max1280x900/462111043.jpg?k=9f846f358bbe1f8f16d9a7146147e5a0f9683951d7dc3f9892a7556aef956dee&o=&hp=1',
       strong: 'Alax Johnson',
       p: 'Commented on your #Uxtrendz Post: Awesome Post !!! Thank You!'
     },
