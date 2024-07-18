cab0656e56b84089847e1f5a10ea1c0e25fab88f
beingRish <rishabhjuly11@gmail.com>
2024-07-18 07:17:37 -0700

    hjdf

--- src/app/observable/share-replay/share-replay.component.ts
+++ src/app/observable/share-replay/share-replay.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-share-replay',
   templateUrl: './share-replay.component.html',
   styleUrls: ['./share-replay.component.scss']
 })
-export class ShareReplayComponent {
+export class ShareReplayComponent implements OnInit{
+
+  ulr = "https://rxjs-sharereplay-default-rtdb.firebaseio.com/products.json"
+  allProducts: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(this.ulr).subscribe(res => {
+      console.log(res);
+      this.allProducts = res;
+    })
+  }
 
 }
